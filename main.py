3233eefe206a60feec8511287fa8982a84d7a80c
justanotherguy9312 <ijustneedspacefr@outlook.com>
2025-11-11 22:21:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-confirmed = True  # you can change this anytime
+confirmed = False  # you can change this anytime
 
 @app.get("/confirm")
 def get_confirmation():
